4681f067a79354279bab7c455dfb9e68d0b87b38
Chris Thielen <cthielen@netflix.com>
2018-03-27 19:44:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# cypress-runner
+
+### Serves an app, and runs cypress e2e tests
+
+### Usage:
+
+```
+Usage: cypress-runner.js [open|run] [options]
+
+Commands:
+  cypress-runner.js open  Open the cypress UI
+  cypress-runner.js run   Runs the cypress test suite
+
+Options:
+  --help     Show help                                                 [boolean]
+  --version  Show version number                                       [boolean]
+  --path     The path to serve files from                      [default: "dist"]
+  --port     The port to serve files from                        [default: 4000]
+
+Specify either "open" or "run"
+```
+
+### Examples
+
+Run your cypress e2e tests against the SPA located in `dist`:
+
+```
+cypress-runner run --path dist
+```
+
+Opens the cypress UI to interactively run e2d tests against the SPA located in `dist`:
+
+```
+cypress-runner run --path dist
+```
+
---
+++ cypress-runner.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const { resolve }  = require('path');
+const yargs = require('yargs')
+    .usage('Usage: $0 [open|run] [options]')
+    .command('open', 'Open the cypress UI')
+    .command('run', 'Runs the cypress test suite')
+    .demandCommand(1, 'Specify either "open" or "run"')
+    .option('path', {
+      description: 'The path to serve files from',
+      default: 'dist',
+    })
+    .option('port', {
+      description: 'The port to serve files from',
+      default: 4000,
+    }).argv;
+
+const { path, port } = yargs;
+const [ cypressCmd ] = yargs._;
+
+if (!fs.existsSync(path)) {
+  console.error(`${resolve(path)} doesn't exist, can't serve files`);
+  process.exit();
+}
+
+const { fork } = require('child_process');
+const browserSync = fork('./node_modules/.bin/serve', ['-n', '-s', '-p', port, path]);
+const cypress = fork('./node_modules/.bin/cypress', [cypressCmd]);
+
+browserSync.on('exit', () => cypress.kill());
+cypress.on('exit', (code) => { browserSync.kill(); process.exit(code) });
